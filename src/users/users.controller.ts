b9f6f4a52cb8dbd33ccf2c50bff9fd4049bfba61
Daler Abdurahimov <01.tolibovich@gmail.com>
2026-07-01 17:54:10 +0500

    Generate users controller and write simple CRUD operations of user end point

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getAllUsers(): string {
+        return "All users"
+    }
+
+    @Get("about")
+    getAllUsersAbout(): string {
+        return "All users about"
+    }
+
+    @Get("search")
+    getUserSearch(@Query("name") name: string, @Query("age") age: string): string {
+        return `User name: ${name} and age: ${age}`
+    }
+
+    @Get(":id")
+    getUserById(@Param("id") id: string): string {
+
+        if (id === "0") {
+            throw new NotFoundException("ID не может быть 0")
+        }
+        return `User ID: ${id}`
+    }
+
+    @Post()
+    create(@Body() body: any) {
+
+        if (!body.name) {
+            throw new NotFoundException("Name is required")
+        }
+        return {message: "User created", data: body}
+    }
+
+    @Put(":id")
+    update(@Param("id") id: string) {
+        return {message: `User with ID ${id} updated`}
+    }
+
+    @Delete(":id")
+    delete(@Param("id") id: string) {
+        return {message: `User with ID ${id} deleted`}
+    }
+}
